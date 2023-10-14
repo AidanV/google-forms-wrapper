d5918b8db39ed7f6794171b348442e8a1c0c6b88
AidanV <aidanvanduyne@gmail.com>
2023-10-14 00:20:39 -0700

    working form view

--- androidApp/src/main/java/com/example/googleformwrapper/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/googleformwrapper/android/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.googleformwrapper.android
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.ValueCallback
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,6 +12,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.googleformwrapper.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    FormView(Greeting().greet())
                 }
             }
         }
@@ -29,14 +34,36 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun FormView(text: String) {
+    // Declare a string that contains a url
+    val mUrl = "https://docs.google.com/forms/d/e/1FAIpQLSd7oQx2d3xdFwKDWTfZ7pZqgXuXLBnTngQHRCHNe9byfpS1Vg/viewform?usp=sf_link"
+
+    // Adding a WebView inside AndroidView
+    // with layout as full screen
+    AndroidView(factory = {
+        val webView = WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+            webViewClient = WebViewClient()
+            loadUrl(mUrl)
+        }
+        webView.settings.javaScriptEnabled = true
+        webView.evaluateJavascript(
+            "(function() { return ('<html><body>'+document.getElementsByTagName('html')[0].innerHTML+'</body></html>'); })();"
+        ) { value -> println(value) }
+        webView
+    }, update = {
+        it.loadUrl(mUrl)
+    })
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        FormView("Hello, Android!")
     }
 }
